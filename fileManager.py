168b8470139b169677d67c18e42663dd65ec887a
manuelrodriguez <manuel.rodriguez@gorillalogic.com>
2020-02-17 15:47:17 -0600

    Finished Student class

--- fileManager.py
+++ fileManager.py
@@ -1,22 +1,30 @@
-def save_file(object_to_save, path):
-    try:
-        f = open(path, "a")
-        f.write(object_to_save + "\n")
-    except Exception:
-        print("Could not save file")
-    finally:
-        f.close()
+class FileManager:
 
+    def save_file(self, object_to_save, path):
+        saved = False
+        try:
+            f = open(path, "a")
+            f.write(object_to_save + "\n")
+            saved = True
+        except Exception:
+            print("Could not save file")
+        finally:
+            try:
+                f.close()
+            except Exception:
+                print("Could not close file!")
+                saved = False
+        return saved
 
-def read_file(path):
-    result = []
-    try:
-        f = open(path, "r")
-        for a in f.readlines():
-            result.append(a)
-    except Exception:
-        print("Could not read file")
-    finally:
-        f.close()
-    return result
+    def read_file(self, path):
+        result = []
+        try:
+            f = open(path, "r")
+            for a in f.readlines():
+                result.append(a)
+        except Exception:
+            print("Could not read file")
+        finally:
+            f.close()
+        return result
 
--- students.py
+++ students.py
@@ -1,18 +1,43 @@
-students = []
+from fileManager import FileManager
 
 
-def get_students_titlecase():
-    students_titlecase = []
-    for student in students:
-        students_titlecase = student["name"].title()
-    return students_titlecase
+class Students:
+    students = []
+    file_manager = FileManager()
+
+    def get_students_titlecase(self):
+        students_titlecase = []
+        for student in self.students:
+            students_titlecase.append(student["name"].title())
+        return students_titlecase
+
+    def print_students_titlecase(self):
+        return self.get_students_titlecase()
+
+    def add_student(self, name, student_id=123):
+        student = {"name": name, "student_id": student_id}
+        self.students.append(student)
+
+    def read_file(self):
+        try:
+            result = self.file_manager.read_file("students.txt")
+            if result:
+                for student in result:
+                    self.add_student(student)
+        except Exception:
+            print("Exception raised when reading file with Student Data")
+
+    def save_file(self):
+        try:
+            for student in self.students:
+                self.file_manager.save_file(student["name"], "students.txt")
+        except Exception:
+            print("Exception raised when saving file with Student Data")
 
 
-def print_students_titlecase():
-    return get_students_titlecase()
+manuel = Students()
+# manuel.add_student("Manuel")
+# manuel.save_file()
 
-
-def add_student(name, student_id=123):
-    student = {"name": name, "student_id": student_id}
-    students.append(student)
-
+manuel.read_file()
+print(manuel.print_students_titlecase())
